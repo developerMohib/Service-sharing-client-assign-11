6b8e9d7522a99fb1c89f148ff3114e89e33b0b16
Mohib <mohibullahmohim2020@gmail.com>
2024-05-10 11:28:45 +0600

    add service page added

--- src/Component/Carrousel/Carrousel.jsx
+++ src/Component/Carrousel/Carrousel.jsx
@@ -17,7 +17,20 @@ const Carrousel = () => {
         modules={[Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide> 
+        <div
+          className="hidden bg-cover bg-center lg:block relative "
+          style={{
+            backgroundImage: `url('https://images.unsplash.com/photo-1606660265514-358ebbadc80d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80')`,
+            backgroundSize: 'cover'
+          }}
+        >
+            <div className="absolute inset-0 bg-black opacity-90"></div>
+
+             <p className='text-white ' > Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur facilis quam minus quasi sequi id dicta, repellendus dolorem labore placeat. Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique expedita dolorum sapiente inventore nam fuga tempora omnis sed reiciendis ipsam porro cumque neque assumenda obcaecati dolor voluptatem tempore labore aperiam nemo, placeat consectetur itaque ipsum! Dignissimos commodi nemo quaerat? Sapiente cum, delectus iure atque deleniti voluptate saepe placeat fugiat provident veritatis autem nemo! Et voluptas in molestias nostrum eaque, cumque debitis illum, accusantium quasi fugit dicta alias accusamus! Provident ea ducimus quas, ipsam tempore tempora velit fugit veniam rem obcaecati alias doloribus iure, voluptatem eum dolor facilis dicta officia id facere deserunt impedit inventore dolore? Facilis ab repellendus hic ut. </p>
+
+        </div>
+        </SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>
         <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
--- src/Pages/AddService/AddService.jsx
+++ src/Pages/AddService/AddService.jsx
@@ -1,9 +1,110 @@
+import { useContext } from "react";
+import { AuthCustomContext } from "../../Provider/Provider";
 
 const AddService = () => {
+    const {user} = useContext(AuthCustomContext);
+    const providerEmail = user?.email ;
+    const providerName = user?.displayName ;
+    const providerPhoto = user?.photoURL ;
+    console.log(providerPhoto);
     return (
-        <div>
-            <h1>im add service</h1>
-        </div>
+        <div className="my-10" >
+      <h1 className="text-center font-bold my-5 text-2xl underline ">Add a Services</h1>
+      <div className=" p-10 " >
+        <form >
+            
+            {/* form Category and details row */}
+          <div className="md:flex gap-8 my-5 " >
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text">Provider Name</span>
+            </div>
+            <input
+              type="text"
+              defaultValue={providerName}
+              readOnly
+              name="name"
+              className="input input-bordered w-full"
+            />
+          </label>
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text"> Provider Email </span>
+            </div>
+            <input
+              type="email"
+              defaultValue={providerEmail}
+              readOnly
+              name="email"
+              className="input input-bordered w-full"
+            />
+          </label>
+          </div>
+            {/* form name and price row */}
+          <div className="md:flex gap-8 my-5 " >
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text"> Service Name </span>
+            </div>
+            <input
+              type="text"
+              placeholder="Service Name"
+              name="serviceName"
+              className="input input-bordered w-full"
+            />
+          </label>
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text"> Price </span>
+            </div>
+            <input
+              type="number"
+              placeholder="Price"
+              name="price"
+              className="input input-bordered w-full"
+            />
+          </label>
+          </div>
+            {/* form service area and photo url row */}
+          <div className="md:flex gap-8 my-5 " >
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text"> Service Area </span>
+            </div>
+            <input
+              type="text"
+              placeholder="Service Area"
+              name="serviceArea"
+              className="input input-bordered w-full"
+            />
+          </label>
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text">Service Photo URL</span>
+            </div>
+            <input
+              type="text"
+              placeholder="Image URL of the Service "
+              name="serviceUrl"
+              className="input input-bordered w-full"
+            />
+          </label>
+          </div>
+            {/* form Details row */}
+
+          <div className="my-5" >
+          <label className="form-control w-full">
+            <div className="label">
+              <span className="label-text">Description</span>
+            </div>
+            <textarea className="input input-bordered w-full" name="details" id="description" placeholder="Description" ></textarea>
+          </label>
+          </div>
+
+          <input className=" btn border-none btn-block my-10 bg-success " type="submit" value="Add Service" />
+        </form>
+      </div>
+    </div>
     );
 };
 
